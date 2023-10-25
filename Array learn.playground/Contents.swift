dd8d7b3e8c89b1dc8f64d35b2935ee928fa6585f
kabulov <n.kabulov@cft.ru>
2023-10-25 22:01:58 +0600

    map is making smth with each element of array and return a new one.

--- Array learn.playground/Contents.swift
+++ Array learn.playground/Contents.swift
@@ -5,3 +5,22 @@ import Foundation
 let array: Array<String> = ["String1", "String2"]
 
 print(array)
+
+var a = [1, 2, 3, 6, 7]
+
+// a = "string"  error
+
+print(a)
+
+
+var toDoList = ["Take out garbage", "Pay bills", "Cross off finished items"]
+var reversedArray: Array<String> = []
+
+for element in toDoList {
+	reversedArray.insert(element, at: 0)
+}
+print(reversedArray)
+
+var newArray = a.map { x in
+	return x - 1
+}
