bd823c929cff3665b94e12fc3999f6add1b6880b
kabulov <n.kabulov@cft.ru>
2023-10-25 23:05:27 +0600

    made exercise

--- Array learn.playground/Contents.swift
+++ Array learn.playground/Contents.swift
@@ -24,3 +24,53 @@ print(reversedArray)
 var newArray = a.map { x in
 	return x - 1
 }
+
+
+var even: Array<Int> = []
+var odd: Array<Int> = []
+for i in 1...10 {
+	if i%2 == 0 {
+		even.insert(i, at: 0)
+	} else {
+		odd.append(i)
+	}
+}
+
+print(odd, even)
+
+func findHouse(address: Int, length: Int) -> Int {
+	var house = 0
+	var even: Array<Int> = []
+	var odd: Array<Int> = []
+	for i in 1...length*2 {
+		if i%2 == 0 {
+			even.insert(i, at: 0)
+		} else {
+			odd.append(i)
+		}
+	}
+	if address%2 == 0 {
+		for i in 0..<even.count {
+			if address == even[i] {
+				house = odd[i]
+			}
+		}
+	} else {
+		for i in 0..<odd.count {
+			if address == odd[i] {
+				house = even[i]
+			}
+		}
+	}
+	return house
+}
+
+print(findHouse(address: 6, length: 5))
+
+func findHouseEff(address: Int, length: Int) -> Int {
+	var summOf = length*2 + 1
+	return summOf - address
+}
+
+print(findHouseEff(address: 6, length: 5))
+
